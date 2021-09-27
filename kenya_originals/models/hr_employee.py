7459de6cda73574edb484d6e153172490135258a
arpit <goelarpit1997@gmail.com>
2021-09-27 19:01:27 +0530

    employee division, regions & areas

--- kenya_originals/models/hr_employee.py
+++ kenya_originals/models/hr_employee.py
@@ -6,6 +6,5 @@ class HrEmployee(models.Model):
     _inherit = "hr.employee"
 
     division_id = fields.Many2one('division.name', string="Division", copy=False)
-    region_id = fields.Many2one('region.name', string="Region", copy=False)
-    area_id = fields.Many2one('region.areas', string="Area", copy=False)
-    
+    region_ids = fields.Many2many('region.name', string="Regions", copy=False)
+    area_ids = fields.Many2many('region.areas', string="Areas", copy=False)
